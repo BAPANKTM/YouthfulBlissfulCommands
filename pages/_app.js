eb1649e342fb5f3728ce9ecbce09d559b4baa31b
saklekabeer226 <36207137-saklekabeer226@users.noreply.replit.com>
2025-03-08 09:13:45 +0000

    Restored to 'b9aff6d221ff13822676d8902f20a2a3042d7998'
    Replit-Restored-To: b9aff6d221ff13822676d8902f20a2a3042d7998

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,15 @@
-import React from "react";
-import Head from "next/head";
-import "../styles/globals.css";
+
+import React from 'react';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta charSet="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-        <title>TeleSharex</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <title>TeleShare</title>
       </Head>
       <Component {...pageProps} />
     </>
